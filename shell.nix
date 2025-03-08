31850726db40d3cff5cc76331aca6039563b102c
silvarc141 <silvarc141@users.noreply.github.com>
2025-03-08 20:13:33 +0100

    deps for some custom nodes and missing tmpdir for git

--- shell.nix
+++ shell.nix
@@ -1,8 +1,13 @@
-with import <nixpkgs> {}; mkShell {
-  LD_LIBRARY_PATH = "${pkgs.stdenv.cc.cc.lib}/lib:/run/opengl-driver/lib";
-  buildInputs = with pkgs.python312Packages; [
+with import <nixpkgs> {}; mkShell rec {
+  packages = [ 
+    zlib 
+    libGL
+    stdenv.cc.cc
+    glib
+  ] ++ (with pkgs.python312Packages; [
     python
     venvShellHook
-  ];
+  ]);
   venvDir = ".venv";
+  LD_LIBRARY_PATH = "${lib.makeLibraryPath packages}:/run/opengl-driver/lib";
 }
